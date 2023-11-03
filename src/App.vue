a4ecc9de15792e42d5a20e3f39562ab032cd3264
Harcyldo Winkelmann <harcyleo@gmail.com>
2023-11-03 08:10:36 -0400

    Criando novas rotas | Consumindo dados de uma API externa | Adicionando Vuex ao projeto

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,14 @@
 
 <script>
 
+  import axios from 'axios'
   export default {
-
+    created() {
+      axios.get('https://fakestoreapi.com/products')
+           .then(response => {
+            console.log(response.data);
+           })
+    }
   }
   
 </script>
