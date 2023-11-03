5587990da193baffa7e02fc4d27bfc1d68791a01
Harcyldo Winkelmann <harcyleo@gmail.com>
2023-11-03 07:45:19 -0400

    Starting project

---
+++ src/views/Product.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      This is the product page
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'Product-pro',
+  
+    methods: {
+     
+    },
+   
+  }
+  </script>
+  
+  <style lang="scss">
+  </style>
+  
