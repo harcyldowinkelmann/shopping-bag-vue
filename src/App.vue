6c5fbf54045476fc3191e561195dd7aaf5ed91f2
Harcyldo Winkelmann <harcyleo@gmail.com>
2023-11-03 08:38:13 -0400

    Configurando store, actions e mutations

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,9 @@
 
 <script>
 
-  import axios from 'axios'
   export default {
     created() {
-      axios.get('https://fakestoreapi.com/products')
-           .then(response => {
-            console.log(response.data);
-           })
+      this.$store.dispatch('loadProducts');
     }
   }
   
